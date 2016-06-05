f064808d5f85eb02194ade765a58538fa044a3af
Ron Peled <ron@activo.com>
2016-06-05 00:25:27 -0700

    More words. Added statistics. Showing recent query matches count.

--- trie.typeahead.js
+++ trie.typeahead.js
@@ -114,33 +114,64 @@ Trie.prototype = {
     }
 };
 
+var englishTrie = new Trie();
+
 
 var trieAutosuggest = function(myTrie) {
     return function findMatches(q, cb) {
 
         var matches = myTrie.autosuggest(q);
+        $('#matches-count').html(matches.length);
+        $('#matches-count').css('background-color', 'yellow');
 
         cb(matches);
     };
 };
 
 
-var englishTrie = new Trie();
-$.each(allEnglishWords, function (i, str) {
-    englishTrie.insert(str.toLowerCase());
+// Potentially help speed up page load, but a copy is already passed to the typeahead function
+// TODO: try to fix this by adding a callback when the queue reached the end (nextItem == empty)
+
+// function processItems(items, processItem, delay){
+//     delay = delay || 10
+//     var queue = items.slice(0)
+//     function processNextBatch(){
+//         var nextItem,
+//             startTime = +new Date
+//         while(startTime + 100 >= +new Date){
+//             nextItem = queue.shift()
+//             if (!nextItem) return
+//             processItem(nextItem)
+//         }
+//         setTimeout(processNextBatch, delay)
+//     }
+//     processNextBatch()
+// }
+//
+// function addWordToTrie(word) {
+//     englishTrie.insert(word);
+// }
+//
+// processItems(allEnglishWords, addWordToTrie);
+
+$(function() {
+
+    for (var i=0; i<allEnglishWords.length; i++) {
+        englishTrie.insert(allEnglishWords[i]);
+    }
+
+    $('.typeahead').typeahead({
+            hint: true,
+            highlight: true,
+            minLength: 1
+        },
+        {
+            name: 'states',
+            limit: 15,
+            source: trieAutosuggest(englishTrie)
+        });
+
+    $('#list-size').html(allEnglishWords.length);
+    $('#trie-node-size').html(englishTrie.countNodes());
+
 });
-
-$('.typeahead').typeahead({
-        hint: true,
-        highlight: true,
-        minLength: 1
-    },
-    {
-        name: 'states',
-        limit: 15,
-        source: trieAutosuggest(englishTrie)
-    });
-
-
-$('#list-size').html(allEnglishWords.length);
-$('#trie-node-size').html(englishTrie.countNodes());
